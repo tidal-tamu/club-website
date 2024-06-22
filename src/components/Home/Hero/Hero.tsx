4f8cfe8a64bcec3ade23ce2d578e25037de4a3b4
minim0no <ahmed.idrees3110@gmail.com>
2024-06-21 18:35:19 -0600

    fix hero flexbox

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -2,14 +2,14 @@ import "./HeroBackground.css";
 
 export default function Hero() {
     return (
-        <div className="w-full flex flex-col flex-1 justify-start items-center font-mont px-5 gap-8 lg:gap-14 backgroundImage">
+        <div className="h-full w-full flex flex-col grow justify-start items-center font-mont px-5 gap-8 lg:gap-14 backgroundImage">
             <h1 className="text-navy font-bold text-6xl xl:text-7xl 2xl:text-8xl text-center pt-20 lg:pt-40">
                 THE AI WAVE STARTS HERE.
             </h1>
             <div className="flex justify-center items-center gap-5">
                 <a
                     href="https://discord.gg/eQ8ScamG4H"
-                    className="flex justify-center items-center flex-shrink-0 gap-3 font-bold text-navy bg-[#EDEAE6] px-3 py-1 lg:px-6 lg:py-2 lg:text-xl rounded-lg border-[1px] border-[#C9C9C9] cursor-pointer hover:bg-[#C9C9C9]"
+                    className="flex justify-center items-center shrink-0 gap-3 font-bold text-navy bg-[#EDEAE6] px-3 py-1 lg:px-6 lg:py-2 lg:text-xl rounded-lg border-[1px] border-[#C9C9C9] cursor-pointer hover:bg-[#C9C9C9]"
                 >
                     Discord
                     <img
@@ -20,7 +20,7 @@ export default function Hero() {
                 </a>
                 <a
                     href="/hackathon"
-                    className="flex justify-center items-center flex-shrink-0 gap-3 font-bold text-white bg-navy px-3 py-1 lg:px-6 lg:py-2 lg:text-xl rounded-lg border-[1px] cursor-pointer border-[#0094D3] hover:bg-[#0094D3]"
+                    className="flex justify-center items-center shrink-0 gap-3 font-bold text-white bg-navy px-3 py-1 lg:px-6 lg:py-2 lg:text-xl rounded-lg border-[1px] cursor-pointer border-[#0094D3] hover:bg-[#0094D3]"
                 >
                     Hackathon
                     <img
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,7 +6,7 @@ import Navbar from "../Navbar";
 export default function Home() {
     return (
         <>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col h-full">
                 <Navbar />
                 <Hero />
             </div>
