2445dd5897f35e3522424cf716e4c7c2a3ba3dad
minim0no <ahmed.idrees3110@gmail.com>
2024-07-04 17:03:48 -0600

    Make footer responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home/Home";
-import Footer from "./components/Footer";
 
 export default function App() {
     return (
@@ -10,7 +9,6 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
-                <Footer />
             </Router>
         </React.StrictMode>
     );
