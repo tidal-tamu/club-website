2264d0608fe7fb4e50be46ce0c0da2daa24d0d8f
minim0no <ahmed.idrees3110@gmail.com>
2024-07-04 17:37:19 -0600

    about responsive

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -4,19 +4,19 @@ export default function About() {
             className="w-full flex flex-col justify-center items-center gap-16 bg-lightBlue font-mont overflow-x-clip"
             id="about-us"
         >
-            <h1 className="text-navy text-6xl xl:text-7xl 2xl:text-8xl font-bold text-center my-6 text-nowrap">
+            <h1 className="text-navy text-6xl lg:text-7xl xl:text-8xl font-bold text-center my-6 text-nowrap">
                 ABOUT US
             </h1>
-            <div className="xl:min-w-[1024px] relative flex flex-col xl:flex-row-reverse justify-center items-center gap-10 bg-white rounded-3xl mx-32 flex-0 px-8 py-10 xl:px-24 xl:py-36">
+            <div className=" md:min-w-[768px] lg:min-w-[1024px] xl:min-w-[1280px] relative flex flex-col md:flex-row-reverse justify-center items-center gap-10 xl:gap-16 bg-white rounded-3xl mx-32 flex-0 px-8 py-10 md:px-12 md:py-20 lg:px-24 lg:py-36 transition-all  ">
                 <img
                     src="./icons/yellow-star.svg"
-                    className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-[100px] xl:w-[200px]"
+                    className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-[100px] lg:w-[200px]"
                 />
                 <img
                     src="./images/officers.jpg"
-                    className="rounded-3xl w-[300px] xl:w-[470px]"
+                    className="rounded-3xl w-[300px] lg:w-[400px] xl:w-[470px]"
                 />
-                <p className="flex justify-center items-center text-center font-medium px-3 xl:text-2xl w-[275px] xl:w-[500px]">
+                <p className="flex justify-center items-center text-center font-medium px-3 md:text-sm lg:text-xl xl:text-2xl w-[275px] lg:w-[500px]">
                     The Tethered Informatics and Data Analytics Lab (TIDAL) is a
                     student-led organization at Texas A&M University dedicated
                     to providing college and high school students with essential
@@ -26,7 +26,7 @@ export default function About() {
                 </p>
                 <img
                     src="./icons/red-star.svg"
-                    className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 w-[125px] xl:w-[250px] z-10"
+                    className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 w-[125px] lg:w-[250px] z-10"
                 />
             </div>
         </div>
