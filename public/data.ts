6b63c3fe1d1d191879028243cdcc58ad3a94fc85
Matthewtershi <matthewtershi@gmail.com>
2024-09-20 15:20:18 -0500

    filled text, animation still buggy ill expand the container later im hungry

--- public/data.ts
+++ public/data.ts
@@ -1,25 +1,23 @@
+import myImage from "../public/icons/logos/tidal-transparent.png";
+
 export const titles = [
     "Hackathons",
-    "",
     "Workshops",
-    "",
     "Projects",
-    "",
     "Speaker Events",
-    "",
-    "Learning Material",
+    "What if I don't have a team?",
     "",
   ];
   
   export const bodies = [
-    "1",
-    "img",
-    "3",
-    "img",
-    "5",
-    "img",
-    "7",
-    "img",
-    "9",
-    "img",
+    "Join a team and develop a unique project to create a program relating to a specific prompt. Show off your skills to company sponsors, meet new people and leave with the experience of a lifetime!",
+    // "img",
+    "Learn and apply the critical skills to build your data science and machine learning career with TIDAL workshops!",
+    // "img",
+    "Join a team to develop a unqiue project over the semester, collaborating with industry professionals, A&M professors, or other students on developing unique AI oslutions to existing issues.",
+    // "img",
+    "Join researchers and professionals from around the globe in learning job-critical skills and exploring new advancements in machine learning.",
+    // "img",
+    "We'll make one for you with our matching bot!",
+    myImage,
   ];
--- src/components/Home/WhatWeDo/parallax-scroll.tsx
+++ src/components/Home/WhatWeDo/parallax-scroll.tsx
@@ -4,6 +4,10 @@ import { useRef } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../../../../lib/utils";
 
+const isImage = (url: string) => {
+  return /\.(jpeg|jpg|gif|png|svg)$/.test(url);
+};
+
 export const ParallaxScroll = ({
   titles,
   bodies,
@@ -15,13 +19,13 @@ export const ParallaxScroll = ({
 }) => {
   const gridRef = useRef<any>(null);
   const { scrollYProgress } = useScroll({
-    container: gridRef, // remove this if your container is not fixed height
-    offset: ["start start", "end start"], // remove this if your container is not fixed height
+    container: gridRef,
+    offset: ["start start", "end start"],
   });
 
-  const translateFirst = useTransform(scrollYProgress, [0, 1], [150, -1000]);
-  const translateSecond = useTransform(scrollYProgress, [0, 1], [50, 200]);
-  const translateThird = useTransform(scrollYProgress, [0, 1], [0, -500]);
+  const translateFirst = useTransform(scrollYProgress, [0, 1], [250, -900]);
+  const translateSecond = useTransform(scrollYProgress, [0, 1], [-50, 150]);
+  const translateThird = useTransform(scrollYProgress, [0, 1], [100, -900]);
 
   const third = Math.ceil(titles.length / 3);
 
@@ -30,51 +34,64 @@ export const ParallaxScroll = ({
   const thirdPart = titles.slice(2 * third);
 
   const bodyFirstPart = bodies.slice(0, third);
-  const bodySecondPart = bodies.slice(third, 2 * third);
+  const bodySecondPart = bodies.slice(third, 2 * third + 1);
   const bodyThirdPart = bodies.slice(2 * third);
 
   return (
     <div
-      className={cn("scrollbar-hidden h-[80rem] items-start overflow-y-auto w-full", className)}
+      className={cn("scrollbar-hidden h-[80rem] items-start overflow-y-clip w-full", className)}
       ref={gridRef}
     >
       <div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start max-w-7xl mx-auto gap-10 py-40"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start max-w-7xl mx-auto py-40"
         ref={gridRef}
       >
-        <div className="grid gap-10">
+        <div className="grid gap-y-80 gap-x-10">
           {firstPart.map((title, idx) => (
             <motion.div
-              style={{ y: translateFirst }} // Apply the translateY motion value here
+              style={{ y: translateFirst }} 
               key={"grid-1" + idx}
-              className="bg-blue-100 m-3 p-10 h-96 w-25 rounded-full"
+              className="bg-blue-100 gap-y-25 m-3 p-10 h-96 w-25 rounded-full flex flex-col justify-center items-center"
             >
               <h2 className="text-lg font-bold text-center">{title}</h2>
               <p className="text-center">{bodyFirstPart[idx]}</p>
             </motion.div>
           ))}
         </div>
-        <div className="grid gap-10">
+        <div className="grid gap-y-40 gap-x-10">
           {secondPart.map((title, idx) => (
             <motion.div
-              style={{ y: translateSecond }}
+              style={{ y: translateSecond }} 
               key={"grid-2" + idx}
-              className="bg-blue-100 m-3 p-10 h-96 w-25 rounded-full"
+              className="bg-blue-100 gap-y-25 m-3 p-10 h-96 w-25 rounded-full flex flex-col justify-center items-center"
             >
               <h2 className="text-lg font-bold text-center">{title}</h2>
               <p className="text-center">{bodySecondPart[idx]}</p>
             </motion.div>
           ))}
         </div>
-        <div className="grid gap-10">
+        <div className="grid gap-y-120 gap-x-10">
           {thirdPart.map((title, idx) => (
             <motion.div
-              style={{ y: translateThird }}
+              style={{ y: translateThird }} 
               key={"grid-3" + idx}
-              className="bg-blue-100 m-3 p-10 h-96 w-25 rounded-full"
+              className="bg-blue-100 gap-y-25 m-3 p-10 h-96 w-25 rounded-full flex flex-col justify-center items-center"
             >
-              <h2 className="text-lg text-center font-bold">{title}</h2>
-              <p className="text-center">{bodyThirdPart[idx]}</p>
+              <h2 className="text-lg font-bold text-center">{title}</h2>
+              
+              {isImage(bodyThirdPart[idx]) ? (
+                <div
+                className="h-full w-full bg-cover bg-center rounded-full"
+                style={{ 
+                  backgroundImage: `url(${bodyThirdPart[idx]})`,
+                  backgroundSize: 'contain',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                }}
+                />
+              ) : (
+                <p className="text-center">{bodyThirdPart[idx]}</p>
+              )}
             </motion.div>
           ))}
         </div>
