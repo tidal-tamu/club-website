ffd19d1717117f3dd1fd2856e129dd6349accc67
Matthewtershi <matthewtershi@gmail.com>
2025-02-06 00:59:08 -0600

    schedule spacing

--- src/components/Hackathon/Schedule/ScheduleList.tsx
+++ src/components/Hackathon/Schedule/ScheduleList.tsx
@@ -20,14 +20,24 @@ export default function ScheduleList({ list, day }: ScheduleListProps) {
                         className="flex items-center relative pb-6"
                     >
                         <div className="bg-[white] hover:bg-gray-300 transition-transform transform hover:scale-105 border border-gray-400 rounded-md p-4 flex-1 relative z-[1]">
-                            <div className="font-mont h-[3vh] flex justify-between items-center transition-colors relative">
+                            {/* <div className="font-mont h-[3vh] flex justify-between items-center transition-colors relative">
                                 <div className="text-sm sm:text-base md:text-lg lg:text-xl font-medium whitespace-nowrap">
                                     {item["time"]}
                                 </div>
 
-                                <div className="border-l-2 border-black h-[50px] absolute left-1/2 transform -translate-x-[12vw] translate-y-[27%] hidden lg:block "></div>
+                                <div className="border-l-2 border-black h-[50px] absolute left-1/2 transform -translate-x-[12vw] translate-y-[27%] hidden lg:block mix-blend-difference"></div>
 
                                 <div className="text-sm sm:text-base md:text-lg font-medium whitespace">
+                                    {item["event"]}
+                                </div>
+                            </div> */}
+                            <div className="font-mont h-[3vh] flex justify-between items-center transition-colors relative">
+                                <div className="relative pl-8 text-sm sm:text-base md:text-lg lg:text-xl font-medium whitespace-nowrap">
+                                    <div className="border-l-2 border-black h-[50px] absolute left-1 top-[100%] transform -translate-y-1/2"></div>
+                                    {item["time"]}
+                                </div>
+
+                                <div className="text-sm sm:text-base md:text-lg font-medium whitespace-nowrap">
                                     {item["event"]}
                                 </div>
                             </div>
