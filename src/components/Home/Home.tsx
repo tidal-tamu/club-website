7de1efbf5c1d2d36f0967ad65ccf39b36fb74252
Matthewtershi <matthewtershi@gmail.com>
2025-09-03 17:59:02 -0500

    oops

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             <About />
             <WhatWeDo />
             <Officers/>
-            {/* <PhotoGallery/> */}
+            <PhotoGallery/>
             <Footer />
         </>
     );
