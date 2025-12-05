7cf0b0ab064c460036efa543752c9d6611eaa092
Isaac Chacko <isaac.chacko05@gmail.com>
2025-12-04 20:48:17 -0600

    Revert "update registration form with guards, year, snow effect"
    This reverts commit 616d90e25b12f53d7d3a89787e3673c12543747e.

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,30 +1,27 @@
 import Navbar from '../HackathonS26/Navbar';
-import { useEffect } from 'react';
+import { useEffect} from 'react';
 import Form from './Form';
-import FloatingParticles from '../HackathonS26/ui/FloatingParticles';
 
 export default function Register() {
 
-  useEffect(() => {
-    document.body.style.backgroundColor = "#76A4C5";
-  }, []);
+    useEffect(() => {
+        document.body.style.backgroundColor = "#76A4C5";
+    }, []);
 
-  return (
-    <div
-      className="min-h-screen hero-gradient relative overflow-hidden"
-      style={{
-        backgroundColor: '#76A4C5'
-      }}
-    >
-      <FloatingParticles count={48} />
+    return (
+        <div 
+            className="min-h-screen hero-gradient relative overflow-hidden"
+            style={{
+                backgroundColor: '#76A4C5'
+            }}
+        >
+            <div className="relative z-30">
+                <Navbar dark />
+            </div>
 
-      <div className="relative z-30">
-        <Navbar dark />
-      </div>
-
-      <div className="relative z-20">
-        <Form />
-      </div>
-    </div>
-  );
+            <div className="relative z-20">
+                <Form/>
+            </div>
+        </div>
+    );
 };
