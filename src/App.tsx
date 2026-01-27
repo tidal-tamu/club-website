486319c495cd0302c18d9b4b72c9fbecd4d3a32a
minim0no <ahmedlol445@tamu.edu>
2026-01-26 18:05:12 -0600

    add discord path

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,34 @@ import HackathonF25 from "./components/HackathonF25/Hackathon";
 import SponsorUs from "./components/Sponsor-Us/SponsorUs";
 import CheckRegistration from "./components/Register/CheckRegistration";
 import Register from "./components/Register/Register";
+import RedirectExternal from "./lib/RedirectExternal";
 
 export default function App() {
-  return (
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sponsor-us" element={<SponsorUs />} />
+    return (
+        <React.StrictMode>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sponsor-us" element={<SponsorUs />} />
 
-          <Route path="/hackathon" element={<HackathonS26 />} />
-          <Route path="/HackathonS26" element={<HackathonS26 />} />
-          <Route path="/HackathonS25" element={<HackathonS25 />} />
-          <Route path="/HackathonF25" element={<HackathonF25 />} />
+                    <Route path="/hackathon" element={<HackathonS26 />} />
+                    <Route path="/HackathonS26" element={<HackathonS26 />} />
+                    <Route path="/HackathonS25" element={<HackathonS25 />} />
+                    <Route path="/HackathonF25" element={<HackathonF25 />} />
 
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/check-registration"
-            element={<CheckRegistration />}
-          />
-        </Routes>
-      </Router>
-    </React.StrictMode>
-  );
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/check-registration"
+                        element={<CheckRegistration />}
+                    />
+                    <Route
+                        path="/discord"
+                        element={
+                            <RedirectExternal url="https://tx.ag/tidaldiscord" />
+                        }
+                    />
+                </Routes>
+            </Router>
+        </React.StrictMode>
+    );
 }
